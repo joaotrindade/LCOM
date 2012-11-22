754de34bfff94663f61e7fdc8928083f4e494849
ei11146 <ei11146@28693d94-a35e-45cb-be99-0ada56eed7dc>
2012-11-22 12:03:47 +0000

    Correcao Lab4

---
+++ lab4/lab4.c
@@ -0,0 +1,13 @@
+#include "test4.h"
+
+#define WAIT_TIME 2
+int main()
+{
+	sef_startup();
+	//printf("Entrou\n");
+	test_scan();
+
+
+	//printf("Saiu\n");
+	return 0;
+}
---
+++ lab4/test4.c
@@ -0,0 +1,82 @@
+#include "test4.h"
+
+static int hook= 0;
+unsigned char scancode;
+
+int kbc_subscribe_int(void) {
+	sys_irqsetpolicy(KB_IRQ, (IRQ_REENABLE | IRQ_EXCLUSIVE), &hook);
+	sys_irqenable(&hook);
+	//printf("FEZ SUBSCRIBE\n");
+	return 1<<0;
+
+}
+
+int kbc_unsubscribe_int(void) {
+	sys_irqdisable(&hook);
+}
+
+long read_scancode() {
+	long stat, data;
+
+	//printf("Entrou read_scancode\n");
+
+	while(1)
+	{
+		sys_inb(STAT_REG, &stat);
+		if (stat & OBF)
+		{
+			sys_inb(OUT_BUF, &data);
+			if ( (stat & (PAR_ERR | TO_ERR)) == 0)
+				return data;
+			else
+				return -1;
+
+		}
+		tickdelay(micros_to_ticks(WAIT_KBC));
+	}
+	//printf("Saiu read_scancode\n");
+
+}
+
+int test_scan(void) {
+
+	int ipc_status, irq_set, esc_found;
+	message msg;
+	esc_found = 0;
+	irq_set = kbc_subscribe_int();
+
+
+
+	while(!esc_found) {
+		if(driver_receive(ANY, &msg, &ipc_status) != 0) {
+			printf("driver_receive failed\n");
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) {
+			/* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+				case HARDWARE :
+					if (msg.NOTIFY_ARG & irq_set)
+					{
+						//printf("entrou ciclo\n");
+						scancode = read_scancode();
+						if (scancode == 0x81) {
+							/* ESCAPE FOUND */
+							esc_found = 1;
+						}
+						if (scancode >> 7 == 1)
+						{
+							printf("Breakcode: 0x%2x \n", scancode);
+						}
+						else printf("Makecode: 0x%2x \n\n",scancode);
+					}
+			}
+		}
+
+	}
+	kbc_unsubscribe_int();
+}
+
+int test_leds(unsigned short n, unsigned short *leds) {
+    /* To be completed */
+}
